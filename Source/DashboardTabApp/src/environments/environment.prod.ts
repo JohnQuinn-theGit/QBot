58b5aa568d420d08479b2fe0757b2dfc6567969a
JohnQuinn-theGit <30435851+JohnQuinn-theGit@users.noreply.github.com>
2021-03-10 20:02:09 +0000

    Update environment.prod.ts
    apiBase URL change rrdsqbotedu

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://RRDsEDUQBot.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://rrdsqbotedu.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
