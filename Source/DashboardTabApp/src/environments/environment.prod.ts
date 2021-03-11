2c2f1ed9686156717dd3d69b2c4c5b5995e46f4b
JohnQuinn-theGit <30435851+JohnQuinn-theGit@users.noreply.github.com>
2021-03-11 14:24:37 +0000

    Update environment.prod.ts

--- Source/DashboardTabApp/src/environments/environment.prod.ts
+++ Source/DashboardTabApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://rrdsqbot.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://rrdsqbotforedu.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
