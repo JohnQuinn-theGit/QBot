ff2db3b6bd53942d83727776a8d6c52280fb10cd
JohnQuinn-theGit <30435851+JohnQuinn-theGit@users.noreply.github.com>
2021-03-11 14:25:30 +0000

    Update environment.ts

--- Source/QuestionsTabApp/src/environments/environment.ts
+++ Source/QuestionsTabApp/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://rrdsqbot.azurewebsites.net/api/Request/",
-    selfUrl: "https://rrdsqbot-questions.azurewebsites.net",
+    apiBaseUrl: "https://rrdsqbotforedu.azurewebsites.net/api/Request/",
+    selfUrl: "https://rrdsqbotforedu-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
